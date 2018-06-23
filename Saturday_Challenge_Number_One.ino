36bf35bdee5198741f47e1b09fd2b9cae07927b6
Andrew's Macbook Air <andrewfriedman01@gmail.com>
2018-06-23 12:37:03 -0400

    Final Version

--- Saturday_Challenge_Number_One.ino
+++ Saturday_Challenge_Number_One.ino
@@ -9,16 +9,7 @@ void setup() {
 }
 
 void loop() {
-  /*The_Servo.write(100);
-  delay(2000);
-  The_Servo.write(-100);*/
-
-  for (pos = 0; pos <= 360; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    The_Servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 360; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
     The_Servo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
